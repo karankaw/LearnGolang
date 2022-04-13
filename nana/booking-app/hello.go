aa158dda75a20877cf06ab312266b449d89dc8b7
Karan Kaw <karankaw@hotmail.com>
2022-04-13 07:23:46 +0530

    Added Println Print

--- nana/booking-app/hello.go
+++ nana/booking-app/hello.go
@@ -1,13 +1,15 @@
 package main
 
-import "htt"
+//import "net/http"
+import "fmt"
 
-func main1(){
-
+//func main1(){
+func main() {
 	//print("Hello World!")
-	Print("Hello World");
+	fmt.Print("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Print("Welcome to Our conference booking application")
 
 }
-	
 
-
+//go run hello.go
