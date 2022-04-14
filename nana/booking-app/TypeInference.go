5a0c23f06964f54e3ff8fe292bcd170a475989f9
Karan Kaw <karankaw@hotmail.com>
2022-04-14 11:19:54 +0530

    FormatSpecifiers and Type Inference

---
+++ nana/booking-app/TypeInference.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func autoInference(){
+
+	var username string
+	const numTickets float32 = 2.0  //int uint int32 int64 float32 bool
+
+	username = "KK"
+
+	fmt.Printf("Name provided : %v booked %v tickets", username, numTickets)
+
+
+
+}
---
+++ nana/booking-app/formatSpecifiers.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func formatter() {
+	//https://pkg.go.dev/fmt
+
+	var bool1 = true
+	fmt.Printf("%t", bool1)
+}
