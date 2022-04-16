950f78b013cac000d37eb639789623b39dcb9992
Karan Kaw <karankaw@hotmail.com>
2022-04-16 14:38:13 +0530

    Added ArraysAssignment

---
+++ nana/module1/AssignArr2toArr5.go
@@ -0,0 +1,10 @@
+package module1
+
+func AssignArrays(arr1 []int, arr2 []int) bool {
+	arr1 = arr2
+	return true
+}
+
+func Add(a, b int) int {
+	return a + b
+}
---
+++ nana/module1/go.mod
@@ -0,0 +1,3 @@
+module sample.com/module1
+
+go 1.17
